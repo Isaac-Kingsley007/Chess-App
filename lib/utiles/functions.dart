c1653c2fa128a21945e6352d2e0babeb142123a3
Isaac-Kingsley007 <160049323+Isaac-Kingsley007@users.noreply.github.com>
2025-01-21 23:10:00 +0530

    initial Changes changed functions.dart getAsset

--- lib/utiles/functions.dart
+++ lib/utiles/functions.dart
@@ -1,36 +1,35 @@
-import 'package:chess_app/Chess/piece.dart';
-import 'package:chess_app/Chess/piece_type.dart';
 import 'package:flutter/material.dart';
+import 'package:chess/chess.dart' as chess_api;
 
-Image getAsset(Piece piece) {
-  if (piece.isWhite) {
-    switch (piece.pieceType) {
-      case PieceType.pawn:
+Image getAsset(chess_api.Piece piece) {
+  if (piece.color == chess_api.Chess.WHITE) {
+    switch (piece.type) {
+      case chess_api.PieceType.PAWN:
         return Image.asset("Chess_plt60.png");
-      case PieceType.knight:
+      case chess_api.PieceType.KNIGHT:
         return Image.asset("Chess_nlt60.png");
-      case PieceType.bishop:
+      case chess_api.PieceType.BISHOP:
         return Image.asset("Chess_blt60.png");
-      case PieceType.rook:
+      case chess_api.PieceType.ROOK:
         return Image.asset("Chess_rlt60.png");
-      case PieceType.queen:
+      case chess_api.PieceType.QUEEN:
         return Image.asset("Chess_qlt60.png");
-      case PieceType.king:
+      case chess_api.PieceType.KING:
         return Image.asset("Chess_klt60.png");
     }
   } else {
-    switch (piece.pieceType) {
-      case PieceType.pawn:
+    switch (piece.type) {
+      case chess_api.PieceType.PAWN:
         return Image.asset("Chess_pdt60.png");
-      case PieceType.knight:
+      case chess_api.PieceType.KNIGHT:
         return Image.asset("Chess_ndt60.png");
-      case PieceType.bishop:
+      case chess_api.PieceType.BISHOP:
         return Image.asset("Chess_bdt60.png");
-      case PieceType.rook:
+      case chess_api.PieceType.ROOK:
         return Image.asset("Chess_rdt60.png");
-      case PieceType.queen:
+      case chess_api.PieceType.QUEEN:
         return Image.asset("Chess_qdt60.png");
-      case PieceType.king:
+      case chess_api.PieceType.KING:
         return Image.asset("Chess_kdt60.png");
     }
   }
